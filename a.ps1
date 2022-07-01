7ab00a58fa31a154a0a4ab953c90cac66ab17a0f
boopidoop <daniel.summerside@yahoo.com>
2022-07-01 15:26:58 +0100

    test

--- a.ps1
+++ a.ps1
@@ -2,8 +2,9 @@ echo "Comitting to Git"
 git config --global user.name "boopidoop"
 git config --global user.email "daniel.summerside@yahoo.com"
 git add .
-git commit -m "$Date : $Changelog"
+git commit -m "test"
 
-git status >> log.txt
+git status > log.txt
+git log >> log.txt
 
 git reset
